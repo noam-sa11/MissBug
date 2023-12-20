dde56e52ee2d808635228bcb554f60b7910fe3ed
Noam Saar <noamsaar11@gmail.com>
2023-12-20 17:30:22 +0200

    Refactor AppHeader

--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,13 +1,52 @@
-const { NavLink } = ReactRouterDOM
+import { LoginSignup } from './LoginSignup.jsx'
+import { userService } from '../services/user.service.js'
+import { showErrorMsg } from '../services/event-bus.service.js'
+
+const { Link, NavLink } = ReactRouterDOM
+const { useNavigate } = ReactRouter
+const { useState } = React
 
 export function AppHeader() {
+  const navigate = useNavigate()
+
+  const [user, setUser] = useState(userService.getLoggedInUser())
+
+  function onLogout() {
+    userService.logout()
+      .then(() => {
+        onSetUser(null)
+      })
+      .catch((err) => {
+        showErrorMsg('OOPs try again')
+      })
+  }
+
+  function onSetUser(user) {
+    console.log("user:", user)
+    setUser(user)
+    navigate('/')
+  }
   return (
     <header className="app-header full grid column align-center">
-      <h1>Bugs are Forever</h1>
-      <nav className="app-nav">
-        <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
-        <NavLink to="/about">About</NavLink>
-      </nav>
+      <section className="header-container grid">
+        <h1>Bugs are Forever</h1>
+        <nav className="app-nav">
+          <NavLink to="/">Home</NavLink> |
+          <NavLink to="/bug">Bugs</NavLink> |
+          <NavLink to="/profile">Profile</NavLink> |
+          <NavLink to="/about">About</NavLink>
+        </nav>
+        {user ? (
+          <section className="login">
+            <Link to={`/user/${user._id}`}>Hello {user.fullname}</Link>
+            <button onClick={onLogout}>Logout</button>
+          </section >
+        ) : (
+          <section className="login">
+            <LoginSignup onSetUser={onSetUser} />
+          </section>
+        )}
+      </section>
     </header>
   )
 }
