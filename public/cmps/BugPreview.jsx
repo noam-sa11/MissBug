9feb214a6cd58d70a46bb4091c1f64631d3d8934
Noam Saar <noamsaar11@gmail.com>
2023-12-20 17:31:16 +0200

    Add creator to Preview

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -13,14 +13,20 @@ export function BugPreview({ bug }) {
             <span>{new Date(bug.createdAt).toLocaleString()}</span>
         </fieldset>
         <fieldset>
+            <legend>Creator: </legend>
+            <span>{bug.creator.fullname}</span>
+        </fieldset>
+        <fieldset>
             <legend>Labels: </legend>
-            <p> 
-                {bug.labels.map((label, idx) => (
-                    <React.Fragment key={idx}>
-                        {label}
-                        {idx < bug.labels.length - 1 && <br />}
-                    </React.Fragment>
-                ))}
+            <p>
+                {bug.labels &&
+                    bug.labels.map((label, idx) => (
+                        <React.Fragment key={idx}>
+                            {label}
+                            {idx < bug.labels.length - 1 && <br />}
+                        </React.Fragment>
+                    ))}
+                {!bug.labels && <span>None</span>}
             </p>
         </fieldset>
 
